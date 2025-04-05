9ec5a968d31cf69c783da19f96c2528490211c00
nooshin <nooshinbry@gmail.com>
2025-04-05 19:09:59 +0330

    define types and interface

--- src/context/FormBuilderContext.tsx
+++ src/context/FormBuilderContext.tsx
@@ -1,10 +1,40 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useReducer, useState } from "react";
 
-const FormBuilderContext = createContext({} as any);
+export type FieldType = "text" | "textarea" | "select" | "checkbox" | "paragraph";
+
+export interface FormField {
+    id: string;
+    type: FieldType;
+    label: string;
+    required?: boolean;
+    options?: string[];
+  }
+
+  type Action = 
+    | { type: "ADD_FIELD"; payload: { fieldType: FieldType } }
+    | { type: "REMOVE_FIELD"; payload: { id: string } }
+    | { type: "UPDATE_FIELD"; payload: {id: string, field: Partial<FormField>} }
+    | { type: "REORDER_FIELDS"; payload: {fromIndex: number; toIndex: number} };
+
+    type State = {
+        fields : FormField[];
+    }
+
+    const initialState: State = { fields: [] };
+
+    function formReducer(state: State, action: Action): State{
+        switch (action.type) {
+            default:
+                return state;
+        }
+    }
+
+const FormBuilderContext = createContext<{state: State, dispatch: React.Dispatch<Action>} | undefined>(undefined);
+
 export const FormBUilderProvider = ({children}: {children: React.ReactNode})=>{
-    const [fields, setField] = useState([] as any[]);
+    const [state, dispatch] = useReducer(formReducer, initialState)
     return (
-        <FormBuilderContext.Provider value={{fields, setField}}>
+        <FormBuilderContext.Provider value={{state, dispatch}}>
             {children}
         </FormBuilderContext.Provider>
     )
