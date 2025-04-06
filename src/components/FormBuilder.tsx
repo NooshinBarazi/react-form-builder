d1e3a44e0ca440d2b19093091e8ab8e58cf49eba
nooshin <nooshinbry@gmail.com>
2025-04-06 17:47:29 +0330

    dispatch add field

--- src/components/FormBuilder.tsx
+++ src/components/FormBuilder.tsx
@@ -1,35 +1,37 @@
+import { useFormBUilder } from "../context/FormBuilderContext";
+import { FIELD_TYPES } from "../types/constants";
+import { Filed } from "./Field";
+
 export const FormBuilder = () => {
+  const { state, dispatch } = useFormBUilder();
+
+  const handleAddField = (type: (typeof FIELD_TYPES)[number]) => {
+    dispatch({ type: "ADD_FIELD", payload: { fieldType: type } });
+  };
+
   return (
     <div className="flex flex-col md:flex-row gap-8 p-8 max-w-6xl mx-auto">
       {/* left form */}
-      <div className="bg-white rounded-xl shadow flex-1 p-6">
-        <h1 className="text-2xl font-bold mb-1">React Form Builder</h1>
-        <p className="text-gray-500 mb-6">
-          Build dynamic forms with the compound component pattern
-        </p>
-        <form>
-          <div className="flex flex-col gap-2 mb-4">
-            <label className="font-medium">name</label>
-            <input type="text" className="border rounded p-2" />
-          </div>
-          <div className="flex flex-col gap-2 mb-4">
-            <label className="font-medium">email</label>
-            <input type="email" className="border rounded p-2" />
-          </div>
-          <button className="p-2 bg-black text-white rounded">submit</button>
-        </form>
-      </div>
-
-      {/* right form */}
       <div className="w-full md:w-64 bg-white rounded-xl shadow p-6">
-        <h1 className="font-medium mb-2">Add Field</h1>
-        <div className="flex flex-col gap-2 mb-4">
-          {["Text", "Textarea", "Select", "Checkbox"].map((field) => (
-            <button key={field} className="border rounded p-2">
-              {field}
+        <h1 className="text-2xl font-bold mb-4">Add Field</h1>
+        <div className="flex flex-col space-y-4">
+          {FIELD_TYPES.map((type) => (
+            <button
+              onClick={() => handleAddField(type)}
+              className="px-4 py-2 bg-blue-600 text-white rounded mb-4"
+            >
+              {type}
             </button>
           ))}
         </div>
+      </div>
+      {/* right form */}
+      <div className="bg-white rounded-xl shadow flex-1 p-6">
+        <h1 className="text-2xl font-bold mb-4">Form Builder</h1>
+
+        {state.fields.map((field) => (
+          <Filed key={field.id} field={field} />
+        ))}
       </div>
     </div>
   );
